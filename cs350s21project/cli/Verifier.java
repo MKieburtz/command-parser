d72f66d13433157ebfd804eb4c6b485a35fd99ac
MKieburtz <michaelkieburtz@gmail.com>
2021-05-20 11:17:19 -0700

    added throwError() method for the verify class

--- cs350s21project/cli/Verifier.java
+++ cs350s21project/cli/Verifier.java
@@ -2,4 +2,7 @@ package cs350s21project.cli;
 
 public class Verifier {
 
+    private static void throwError(String datatype, String badValue) {
+        throw new RuntimeException("Error! Invalid " + datatype + ": " + badValue);
+    }
 }
