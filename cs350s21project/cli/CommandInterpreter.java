aee48e0c680fb13b741015e9592ec072e20d7593
Spencer metz <spartanash1@gmail.com>
2021-06-04 07:31:03 -0700

    Update CommandInterpreter.java
    updating CommandInterpreter.java
    need someone

--- cs350s21project/cli/CommandInterpreter.java
+++ cs350s21project/cli/CommandInterpreter.java
@@ -1,40 +1,32 @@
 package cs350s21project.cli;
 
 public class CommandInterpreter {
-
 	public void evaluate(String command) {
 		try {
-			//String commandCommentsArray[] = command.split(Pattern.quote("[//]"));
+			if(command.isEmpty()) {
+				System.out.println("Command is missing");
+			}
 			
-			String commandArray[] = command.split(";");
+			String[] commandArray = command.split(";"); // Commands may appear on the same line if they are separated by a semicolon.
 			for(String i : commandArray) {
-				String commStr[] = i.split(" +");			
+				String[] commStr = i.split(" +"); // Whitespace, except in literals, does not matter.	
 				if (commStr[0].equalsIgnoreCase("set")) {
-					new SetCommandHandler().handleSetCommand(command, commStr[]);
+					new SetCommandHandler().handleSetCommand(command, commStr);
 				} else if (commStr[0].equalsIgnoreCase("create")) {
-					new CreateCommandHandler().handleCreateCommand(command, commStr[]);
+					new CreateCommandHandler().handleCreateCommand(command, commStr);
 				} else if (commStr[0].equalsIgnoreCase("delete")) {
-					new DeleteCommandHandler().handleDeleteCommand(command, commStr[]);
+					new DeleteCommandHandler().handleDeleteCommand(command, commStr);
 				} else if (commStr[0].equalsIgnoreCase("define")) {
-					new DefineCommandHandler().handleDefineCommand(command, commStr[]);
+					new DefineCommandHandler().handleDefineCommand(command, commStr);
 				} else if (commStr[0].equalsIgnoreCase("@load") || commStr[0].equalsIgnoreCase("@pause") || commStr[0].equalsIgnoreCase("@resume") || commStr[0].equalsIgnoreCase("@set")
 														 || commStr[0].equalsIgnoreCase("@wait") || commStr[0].equalsIgnoreCase("@force") || commStr[0].equalsIgnoreCase("@exit")) {
-					new MiscCommandHandler().handleMiscCommand(command, commStr[]);
+					new MiscCommandHandler().handleMiscCommand(command, commStr);
+				} else if(commStr[0].equals("//")) { // A comment prefixed with // may follow a command or be on its own line.
+					print("\n" + commStr[0] + "\n");
 				}
 			}
 		} catch (RuntimeException e) { // throws RuntimeException for invalid commands
-			System.out.println("Error in evaluate" + e); // TODO work on this once i get methods
+			System.out.println("Invalid Command:" + command + "\n\nError message" + e);
 		}
 	}
 }
-
-/* TO BE REMOVED
- * Whitespace, except in literals, does not matter. All text except identifiers
- * is case insensitive.
- * 
- * Commands may appear on the same line if they are separated by a semicolon.
- * 
- * A comment prefixed with // may follow a command or be on its own line.
- */
-
-//"submits it to the schedule method in CommandManagers." We do this in the methods of the commands themselves.​
