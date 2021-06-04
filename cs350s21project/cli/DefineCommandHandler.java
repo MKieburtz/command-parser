6b81f19c4b9640c13128c36e47839c1850d1decf
roylalog000 <65687194+roylalog000@users.noreply.github.com>
2021-06-03 21:54:08 -0700

    Update DefineCommandHandler.java
    bomb, shell, active sonar, passive sonar

--- cs350s21project/cli/DefineCommandHandler.java
+++ cs350s21project/cli/DefineCommandHandler.java
@@ -1,10 +1,15 @@
 package cs350s21project.cli;
 
 import cs350s21project.controller.CommandManagers;
+
 import cs350s21project.controller.command.actor.CommandActorDefineShip;
+import cs350s21project.controller.command.munition.CommandMunitionDefineBomb;
+import cs350s21project.controller.command.munition.CommandMunitionDefineShell;
 import cs350s21project.controller.command.munition.CommandMunitionDefineTorpedo;
 import cs350s21project.controller.command.sensor.CommandSensorDefineAcoustic;
 import cs350s21project.controller.command.sensor.CommandSensorDefineRadar;
+import cs350s21project.controller.command.sensor.CommandSensorDefineSonarActive;
+import cs350s21project.controller.command.sensor.CommandSensorDefineSonarPassive;
 import cs350s21project.controller.command.sensor.CommandSensorDefineThermal;
 import cs350s21project.datatype.*;
 
@@ -27,9 +32,9 @@ public class DefineCommandHandler {
     private void handleDefineMunitionCommand(String command, String[] parts) {
         Verifier.verifyCommandHasAtLeastNumArguments(command, 3);
         if (parts[2].equalsIgnoreCase("bomb")) {
-
+            defineBomb(command, parts);
         } else if (parts[2].equalsIgnoreCase("shell")) {
-
+            defineShell(command, parts);
         } else if (parts[2].equalsIgnoreCase("depth_charge")) {
 
         } else if (parts[2].equalsIgnoreCase("torpedo")) {
@@ -72,7 +77,12 @@ public class DefineCommandHandler {
         } else if (parts[2].equalsIgnoreCase("acoustic")) {
             defineAcousticSensor(command, parts);
         } else if (parts[2].equalsIgnoreCase("sonar")) {
-
+            Verifier.verifyCommandHasAtLeastNumArguments(command, 4);
+			if(parts[3].equalsIgnoreCase("active")) {
+				defineActiveSonarSensor(command, parts);
+			} else if(parts[3].equalsIgnoreCase("passive")) {
+				definePassiveSonarSensor(command, parts);
+			}
         } else if (parts[2].equalsIgnoreCase("depth")) {
 
         } else if (parts[2].equalsIgnoreCase("distance")) {
@@ -157,4 +167,72 @@ public class DefineCommandHandler {
         CommandActorDefineShip defineShipCommand = new CommandActorDefineShip(CommandManagers.getInstance(), command, agentID, munitionIds);
         CommandManagers.getInstance().schedule(defineShipCommand);
     }
+    
+    private void defineBomb(String command, String[] parts) {
+		Verifier.verifyCommandHasNumArguments(command, 4);
+		//id
+		Verifier.verifyID(parts[3]);
+		AgentID id = new AgentID(parts[3]);
+		
+		//create command
+		CommandMunitionDefineBomb bomb = new CommandMunitionDefineBomb(CommandManagers.getInstance(), command, id);
+		CommandManagers.getInstance().schedule(bomb);
+	}
+	
+	private void defineShell(String command, String[] parts) {
+		Verifier.verifyCommandHasNumArguments(command, 4);
+		//id
+		Verifier.verifyID(parts[3]);
+		AgentID id = new AgentID(parts[3]);
+		
+		//create command
+		CommandMunitionDefineShell shell = new CommandMunitionDefineShell(CommandManagers.getInstance(), command, id);
+		CommandManagers.getInstance().schedule(shell);
+	}
+	
+	private void defineActiveSonarSensor(String command, String[] parts) {
+		Verifier.verifyCommandHasNumArguments(command, 10);
+		//id
+		Verifier.verifyID(parts[4]);
+		AgentID id = new AgentID(parts[4]);
+		//"with power"
+		if(!parts[5].equalsIgnoreCase("with") || !parts[5].equalsIgnoreCase("power")) {
+			throw new RuntimeException("Error! Syntax error on define active sonar sensor command (missing or incorrect \"with power\")");
+		}
+		//power
+		Verifier.verifyPower(parts[7]);
+		double d = Double.parseDouble(parts[7]);
+		Power power = new Power(d);
+		//"sensitivity"
+		if(!parts[8].equalsIgnoreCase("sensitivity")) {
+			throw new RuntimeException("Error! Syntax error on define active sonar sensor command (missing or incorrect \"sensitivity\")");
+		}
+		//sensitivity
+		Verifier.verifySensitivity(parts[9]);
+		d = Double.parseDouble(parts[9]);
+		Sensitivity sens = new Sensitivity(d);
+		
+		//create command
+		CommandSensorDefineSonarActive sensor = new CommandSensorDefineSonarActive(CommandManagers.getInstance(), command, id, power, sens);
+		CommandManagers.getInstance().schedule(sensor);
+	}
+	
+	private void definePassiveSonarSensor(String command, String[] parts) {
+		Verifier.verifyCommandHasNumArguments(command, 8);
+		//id
+		Verifier.verifyID(parts[4]);
+		AgentID id = new AgentID(parts[4]);
+		//"with sensitivity"
+		if(!parts[5].equalsIgnoreCase("with") || !parts[5].equalsIgnoreCase("sensitivity")) {
+			throw new RuntimeException("Error! Syntax error on define passive sonar sensor command (missing or incorrect \"with sensitivity\")");
+		}
+		//sensitivity
+		Verifier.verifySensitivity(parts[7]);
+		double d = Double.parseDouble(parts[7]);
+		Sensitivity sens = new Sensitivity(d);
+		
+		//create command
+		CommandSensorDefineSonarPassive sensor = new CommandSensorDefineSonarPassive(CommandManagers.getInstance(), command, id, sens);
+		CommandManagers.getInstance().schedule(sensor);
+	}
 }
